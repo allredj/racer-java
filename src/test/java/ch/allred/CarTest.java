fd91f594e12a16e3367e28db999dadf12d8cbc35
Joel Allred <joel.allred@gmail.com>
2020-10-31 12:48:23 +0000

    Some simple unit tests for utility methods
    Some of them created by the Diffble plugin.

---
+++ src/test/java/ch/allred/CarTest.java
@@ -0,0 +1,24 @@
+package ch.allred;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+  @Test
+  public void testAngleBetweenVectors() {
+    assertEquals(0.0, Car.signedAngleBetweenVectors(10.0, 10.0, 10.0, 10.0));
+  }
+
+  @Test
+  public void testLateralTyreForce() {
+    assertEquals(0.0, Car.lateralTyreForce(0, 1, 0, 1));
+    assertEquals(1.0, Car.lateralTyreForce(0, 1, 1, 0));
+    assertEquals(-1.0, Car.lateralTyreForce(0, 1, -1, 0));
+    assertEquals(2.0, Car.lateralTyreForce(0, 1, 2, 0));
+    assertEquals(0.0, Car.lateralTyreForce(10.0, 10.0, 10.0, 10.0));
+  }
+
+}
+
