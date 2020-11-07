95e1a8f6c0c0d53684a07a11d852ce43e41b24bb
Joel Allred <joel.allred@gmail.com>
2020-11-07 13:13:26 +0000

    Allow for bottom margin

--- src/main/java/ch/allred/racer/Racer.java
+++ src/main/java/ch/allred/racer/Racer.java
@@ -7,11 +7,12 @@ public class Racer extends JFrame {
 
   private final static int TRACK_WIDTH = 800;
   private final static int TRACK_HEIGHT = 600;
+  private final static int BOTTOM_MARGIN = 28;
 
   private void initUI() {
     add(new Track(TRACK_WIDTH, TRACK_HEIGHT));
     setTitle("Racer");
-    setSize(TRACK_WIDTH, TRACK_HEIGHT);
+    setSize(TRACK_WIDTH, TRACK_HEIGHT + BOTTOM_MARGIN);
     setLocationRelativeTo(null);
     setResizable(false);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
