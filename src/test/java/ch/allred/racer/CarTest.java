637f49b1a405f3b41b1cb2ccc877ee8672ee9b00
Joel Allred <joel.allred@gmail.com>
2020-11-07 12:01:38 +0000

    Tests for signedAngleBetweenVectors

--- src/test/java/ch/allred/racer/CarTest.java
+++ src/test/java/ch/allred/racer/CarTest.java
@@ -8,6 +8,14 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
   @Test
+  public void testSignedAngleBetweenVectors() {
+    assertEquals(0.0, Car.signedAngleBetweenVectors(10.0, 10.0, 10.0, 10.0));
+    assertEquals(Math.PI/2, Car.signedAngleBetweenVectors(0.0, 10.0, 10.0, 0.0));
+    assertEquals(Math.PI, Car.signedAngleBetweenVectors(0.0, 10.0, 0.0, -10.0));
+    assertEquals(-Math.PI/2, Car.signedAngleBetweenVectors(0.0, 10.0, -10.0, 0.0));
+  }
+
+  @Test
   public void testAngleBetweenVectors() {
     Assertions.assertEquals(0.0, Car.signedAngleBetweenVectors(10.0, 10.0, 10.0, 10.0));
   }
