a71f5004ff49dc9f94cdf2ad9aba69765555e6b7
Joel Allred <joel.allred@gmail.com>
2020-11-29 21:00:32 +0000

    Introduce NonPhysicalObject

---
+++ src/main/java/ch/allred/racer/NonPhyiscalObject.java
@@ -0,0 +1,12 @@
+package ch.allred.racer;
+
+public class NonPhyiscalObject extends Sprite {
+
+  public NonPhyiscalObject(final int x, final int y) {
+    super(x, y);
+  }
+
+  public NonPhyiscalObject(final int x, final int y, final int width, final int height) {
+    super(x, y, width, height);
+  }
+}
